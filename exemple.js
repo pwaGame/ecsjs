f8eb0a75e1a13ef58f2d257fccfae5660944383c
Zacchari CARDE <zcarde@student.42.fr>
2019-12-06 16:33:48 +0100

    update acceleration to speed

--- exemple.js
+++ exemple.js
@@ -5,12 +5,12 @@ const System = require('./src/System');
 let player1 = Entity.create({position: {x: 0, y: 0, z: 0}});
 let player2 = Entity.create({position: {x: 0, y: 0, z: 0}});
 let player3 = Entity.create();
-player1.registerComponent('acceleration', {
+player1.registerComponent('speed', {
     x: 1,
     y: 0,
     z: 0,
 });
-player2.registerComponent('acceleration', {
+player2.registerComponent('speed', {
     x: 1,
     y: 0,
     z: 0,
@@ -21,12 +21,12 @@ ECS.registerEntity(player3);
 
 class MoveSystem extends System {
     constructor() {
-        super('acceleration', 'position');
+        super('speed', 'position');
     }
 
     update(dt, entity) {
         let position = entity.position.x;
-        entity.position.x += entity.acceleration.x * (dt / 1000);
+        entity.position.x += entity.speed.x * (dt / 1000);
         if (position !== entity.position.x) {
             entity.updated = true;
         }
